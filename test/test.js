2ccda849e777905370f699bae0e7f669f8a8db62
Takuto Wada <takuto.wada@gmail.com>
2016-06-14 16:15:06 +0900

    test(merge-estraverse-visitors): testing visitors with ease

--- test/test.js
+++ test/test.js
@@ -14,64 +14,102 @@ var code = [
     '}'
 ].join('\n');
 
-it('merge multiple visitors', function () {
-    var logs = [];
-    estraverse.traverse(acorn.parse(code), mergeVisitors(
-        {
-            enter: function (currentNode, parentNode) {
-                switch(currentNode.type) {
-                case 'ForStatement':
-                    logs.push('v1: going to skip ' + currentNode.type);
-                    return estraverse.VisitorOption.Skip;
-                case 'CallExpression':
-                case 'FunctionDeclaration':
-                    logs.push('v1: entering ' + currentNode.type);
-                    break;
-                }
-                return undefined;
-            },
-            leave: function (currentNode, parentNode) {
-                switch(currentNode.type) {
-                case 'ForStatement':
-                case 'CallExpression':
-                case 'FunctionDeclaration':
-                    logs.push('v1: leaving ' + currentNode.type);
-                    break;
-                }
-            }
-        },
-        {
-            enter: function (currentNode, parentNode) {
-                switch(currentNode.type) {
-                case 'ForStatement':
-                case 'CallExpression':
-                case 'FunctionDeclaration':
-                    logs.push('v2: entering ' + currentNode.type);
-                    break;
-                }
-                return undefined;
-            },
-            leave: function (currentNode, parentNode) {
-                switch(currentNode.type) {
-                case 'ForStatement':
-                case 'CallExpression':
-                case 'FunctionDeclaration':
-                    logs.push('v2: leaving ' + currentNode.type);
-                    break;
-                }
-            }
+function visitor (name, when, logs) {
+    return function (currentNode, parentNode) {
+        switch(currentNode.type) {
+        case 'ForStatement':
+        case 'CallExpression':
+        case 'FunctionDeclaration':
+            logs.push(name + ': ' + when + ' ' + currentNode.type);
+            break;
         }
-    ));
-    assert.deepEqual(logs, [
-        'v1: entering FunctionDeclaration',
-        'v2: entering FunctionDeclaration',
-        'v1: going to skip ForStatement',
-        'v2: entering ForStatement',
-        'v2: entering CallExpression',
-        'v2: leaving CallExpression',
-        'v2: leaving ForStatement',
-        'v1: leaving ForStatement',
-        'v2: leaving FunctionDeclaration',
-        'v1: leaving FunctionDeclaration'
-    ]);
+    };
+}
+
+describe('merge multiple visitors', function () {
+    it('visitors in order', function () {
+        var logs = [];
+        estraverse.traverse(acorn.parse(code), mergeVisitors(
+            {
+                enter: visitor('v1', 'entering', logs),
+                leave: visitor('v1', 'leaving', logs)
+            },
+            {
+                enter: visitor('v2', 'entering', logs),
+                leave: visitor('v2', 'leaving', logs)
+            }
+        ));
+        assert.deepEqual(logs, [
+            'v1: entering FunctionDeclaration',
+            'v2: entering FunctionDeclaration',
+            'v1: entering ForStatement',
+            'v2: entering ForStatement',
+            'v1: entering CallExpression',
+            'v2: entering CallExpression',
+            'v2: leaving CallExpression',
+            'v1: leaving CallExpression',
+            'v2: leaving ForStatement',
+            'v1: leaving ForStatement',
+            'v2: leaving FunctionDeclaration',
+            'v1: leaving FunctionDeclaration'
+        ]);
+    });
+    it('enter only visitor, leave only visitor', function () {
+        var logs = [];
+        estraverse.traverse(acorn.parse(code), mergeVisitors(
+            {
+                enter: visitor('v1', 'entering', logs)
+            },
+            {
+                leave: visitor('v2', 'leaving', logs)
+            }
+        ));
+        assert.deepEqual(logs, [
+            'v1: entering FunctionDeclaration',
+            'v1: entering ForStatement',
+            'v1: entering CallExpression',
+            'v2: leaving CallExpression',
+            'v2: leaving ForStatement',
+            'v2: leaving FunctionDeclaration'
+        ]);
+    });
+});
+
+describe('interrupt skip', function () {
+    it('skip per visitor', function () {
+        var logs = [];
+        estraverse.traverse(acorn.parse(code), mergeVisitors(
+            {
+                enter: function (currentNode, parentNode) {
+                    switch(currentNode.type) {
+                    case 'ForStatement':
+                        logs.push('v1: going to skip ' + currentNode.type);
+                        return estraverse.VisitorOption.Skip;
+                    case 'CallExpression':
+                    case 'FunctionDeclaration':
+                        logs.push('v1: entering ' + currentNode.type);
+                        break;
+                    }
+                    return undefined;
+                },
+                leave: visitor('v1', 'leaving', logs)
+            },
+            {
+                enter: visitor('v2', 'entering', logs),
+                leave: visitor('v2', 'leaving', logs)
+            }
+        ));
+        assert.deepEqual(logs, [
+            'v1: entering FunctionDeclaration',
+            'v2: entering FunctionDeclaration',
+            'v1: going to skip ForStatement',
+            'v2: entering ForStatement',
+            'v2: entering CallExpression',
+            'v2: leaving CallExpression',
+            'v2: leaving ForStatement',
+            'v1: leaving ForStatement',
+            'v2: leaving FunctionDeclaration',
+            'v1: leaving FunctionDeclaration'
+        ]);
+    });
 });
