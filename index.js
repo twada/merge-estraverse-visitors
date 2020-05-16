9afa74544f65963f46eaf709797a8a7e7babe9c1
Takuto Wada <takuto.wada@gmail.com>
2020-05-17 00:02:54 +0900

    refactor: modernize codebase

--- index.js
+++ index.js
@@ -10,124 +10,138 @@
  */
 'use strict';
 
-var estraverse = require('estraverse');
-
-function SubVisitor () {
-  this.skipStartNode = null;
-  this.broken = false;
-}
-SubVisitor.prototype.isBroken = function () {
-  return !!this.broken;
-};
-SubVisitor.prototype.markBroken = function () {
-  this.broken = true;
-};
-SubVisitor.prototype.isSkipping = function (controller) {
-  return this.skipStartNode && (this.skipStartNode !== controller.current());
-};
-SubVisitor.prototype.startSkipping = function (controller) {
-  this.skipStartNode = controller.current();
-};
-SubVisitor.prototype.finishSkippingIfLeavingFrom = function (controller) {
-  if (this.skipStartNode === controller.current()) {
-    this.skipStartNode = null;
-  }
-};
+const { VisitorOption } = require('estraverse');
 
 function noop () {
 }
 
+class SubVisitor {
+  constructor ({ enter, leave }) {
+    this.enter = (typeof enter === 'function') ? enter : noop;
+    this.leave = (typeof leave === 'function') ? leave : noop;
+    this.skipStartNode = null;
+    this.broken = false;
+  }
+
+  subEnter (controller, currentNode, parentNode) {
+    return this.enter.call(controller, currentNode, parentNode);
+  }
+
+  subLeave (controller, currentNode, parentNode) {
+    return this.leave.call(controller, currentNode, parentNode);
+  }
+
+  isBroken () {
+    return !!this.broken;
+  }
+
+  markBroken () {
+    this.broken = true;
+  }
+
+  isSkipping (controller) {
+    return this.skipStartNode && (this.skipStartNode !== controller.current());
+  }
+
+  startSkipping (controller) {
+    this.skipStartNode = controller.current();
+  }
+
+  finishSkippingIfLeavingFrom (controller) {
+    if (this.skipStartNode === controller.current()) {
+      this.skipStartNode = null;
+    }
+  }
+}
+
 function createSubVisitors (visitors) {
-  var enters = [];
-  var leaves = [];
-  var subVisitor; var i; var v; var len = visitors.length;
-  for (i = 0; i < len; i += 1) {
-    v = visitors[i];
-    subVisitor = new SubVisitor();
-    subVisitor.enter = (typeof v.enter === 'function') ? v.enter : noop;
-    subVisitor.leave = (typeof v.leave === 'function') ? v.leave : noop;
+  const enters = [];
+  const leaves = [];
+  for (const v of visitors) {
+    const subVisitor = new SubVisitor({ enter: v.enter, leave: v.leave });
     enters.push(subVisitor);
     leaves.unshift(subVisitor);
   }
   return {
-    enters: enters,
-    leaves: leaves
+    enters,
+    leaves
   };
 }
 
 module.exports = function mergeVisitors (visitors) {
-  var subVisitors = createSubVisitors(visitors);
+  const subVisitors = createSubVisitors(visitors);
   return {
     enter: function (currentNode, parentNode) {
-      var orig = this;
-      subVisitors.enters.forEach(function (subVisitor) {
-        var controller = Object.create(orig);
+      const orig = this;
+      for (const subVisitor of subVisitors.enters) {
         if (subVisitor.isBroken()) {
-          return;
+          continue;
         }
-        if (subVisitor.isSkipping(controller)) {
-          return;
+        if (subVisitor.isSkipping(orig)) {
+          continue;
         }
-        controller.notify = function notify (flag) {
+        const controller = Object.create(orig);
+        controller.notify = (flag) => {
           switch (flag) {
-            case estraverse.VisitorOption.Skip:
+            case VisitorOption.Skip:
               subVisitor.startSkipping(controller);
               return;
-            case estraverse.VisitorOption.Break:
+            case VisitorOption.Break:
               subVisitor.markBroken();
               return;
             default:
               orig.notify(flag);
           }
         };
-        var ret = subVisitor.enter.call(controller, currentNode, parentNode);
-        switch (ret) {
-          case estraverse.VisitorOption.Skip:
+        const flag = subVisitor.subEnter(controller, currentNode, parentNode);
+        switch (flag) {
+          case VisitorOption.Skip:
             subVisitor.startSkipping(controller);
             break;
-          case estraverse.VisitorOption.Break:
+          case VisitorOption.Break:
             subVisitor.markBroken();
             break;
         }
-      });
+      }
     },
     leave: function (currentNode, parentNode) {
-      var orig = this;
-      var replacements = [];
-      subVisitors.leaves.forEach(function (subVisitor) {
-        var controller = Object.create(orig);
+      const orig = this;
+      const replacements = [];
+      for (const subVisitor of subVisitors.leaves) {
         if (subVisitor.isBroken()) {
-          return;
+          continue;
         }
-        if (subVisitor.isSkipping(controller)) {
-          return;
+        if (subVisitor.isSkipping(orig)) {
+          continue;
         }
+        const controller = Object.create(orig);
         subVisitor.finishSkippingIfLeavingFrom(controller);
-        controller.notify = function notify (flag) {
+        controller.notify = (flag) => {
           switch (flag) {
-            case estraverse.VisitorOption.Skip:
+            case VisitorOption.Skip:
               // subVisitor.startSkipping(controller);  // meaningless
               return;
-            case estraverse.VisitorOption.Break:
+            case VisitorOption.Break:
               subVisitor.markBroken();
               return;
             default:
               orig.notify(flag);
           }
         };
-        var ret = subVisitor.leave.call(controller, currentNode, parentNode);
+        const ret = subVisitor.subLeave(controller, currentNode, parentNode);
         switch (ret) {
-          case estraverse.VisitorOption.Skip:
+          case VisitorOption.Skip:
             // subVisitor.startSkipping(controller);  // meaningless
-            return;
-          case estraverse.VisitorOption.Break:
+            continue;
+          case VisitorOption.Break:
             subVisitor.markBroken();
-            return;
+            continue;
         }
         if (typeof ret === 'object' && ret !== null && typeof ret.type === 'string') {
+          // Node replacement
           replacements.push(ret);
         }
-      });
+      }
       if (replacements.length === 1) {
         return replacements[0];
       }
